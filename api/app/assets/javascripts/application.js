536f83b4b1710f119d8810c8ba6c64dd0dcdae83
Kimberly Cabbagestalk <kimcabb@gmail.com>
2016-04-05 09:13:46 -0400

    updates

--- api/app/assets/javascripts/application.js
+++ api/app/assets/javascripts/application.js
@@ -13,4 +13,72 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
+
+"use strict";
+
+(function(){
+  angular
+  .module("whereismystuff",[
+    "ui.router",
+    "welcome",
+    "createProfile",
+    "userProfile",
+    "lostItem",
+    "itemReceived",
+    "reportFoundItem",
+    "thanksForReporting"
+  ])
+
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ]);
+
+  function RouterFunction ($stateProvider) {
+    $stateProvider
+    .state("homeIndex", {
+      url: "/",
+      templateUrl: "js/stuff/home.html",
+      controller: "stuffIndexController",
+      controllerAs: "stuffIndexViewModel"
+    })
+
+    .state("createProfileIndex", {
+      url: "/createprofile",
+      templateUrl: "js/stuff/createprofile.html",
+      controller: "createProfileController",
+      controllerAs: "createProfileViewModel"
+    })
+    .state("userProfileIndex", {
+      url: "/userprofile",
+      templateUrl: "js/stuff/userprofile.html",
+      controller: "userProfileController",
+      controllerAs: "userProfileViewModel"
+    })
+    .state("lostItemIndex", {
+      url: "/lostitem",
+      templateUrl: "js/stuff/lostitem.html",
+      controller: "lostItemController",
+      controllerAs: "lostItemViewModel"
+    })
+    .state("itemReceivedIndex", {
+      url: "/itemreceived",
+      templateUrl: "js/stuff/itemreceived.html",
+      controller: "itemReceivedController",
+      controllerAs: "itemReceivedViewModel"
+    })
+    .state("reportFoundItem", {
+      url: "/reportfounditem",
+      templateUrl: "js/stuff/reportfounditem.html",
+      controller: "reportFoundItemController",
+      controllerAs: "reportFoundItemViewModel"
+    })
+    .state("thanksForReporting", {
+      url: "/thanksforreporting",
+      templateUrl: "js/stuff/thanksforreporting.html",
+      controller: "thanksForReportingController",
+      controllerAs: "thanksForReportingViewModel"
+    })
+    ;
+  }
+  })();
